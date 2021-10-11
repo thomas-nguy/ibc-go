a94056151d5a260aed144c6d0375326668d81f96
Thomas Nguy <thomas.nguy@crypto.com>
2021-10-11 13:59:07 +0900

    support hooks in transfer app

--- modules/apps/transfer/keeper/relay.go
+++ modules/apps/transfer/keeper/relay.go
@@ -110,7 +110,8 @@ func (k Keeper) SendTransfer(
 	// chain inside the packet data. The receiving chain will perform denom
 	// prefixing as necessary.
 
-	if types.SenderChainIsSource(sourcePort, sourceChannel, fullDenomPath) {
+	isSource := types.SenderChainIsSource(sourcePort, sourceChannel, fullDenomPath)
+	if isSource {
 		labels = append(labels, telemetry.NewLabel(coretypes.LabelSource, "true"))
 
 		// create the escrow address for the tokens
@@ -178,6 +179,9 @@ func (k Keeper) SendTransfer(
 		)
 	}()
 
+	if k.hooks != nil {
+		k.hooks.AfterSendTransfer(ctx, sourcePort, sourceChannel, token, sender, receiver, isSource)
+	}
 	return nil
 }
 
@@ -221,7 +225,8 @@ func (k Keeper) OnRecvPacket(ctx sdk.Context, packet channeltypes.Packet, data t
 	// chain would have prefixed with DestPort and DestChannel when originally
 	// receiving this coin as seen in the "sender chain is the source" condition.
 
-	if types.ReceiverChainIsSource(packet.GetSourcePort(), packet.GetSourceChannel(), data.Denom) {
+	isSource := types.ReceiverChainIsSource(packet.GetSourcePort(), packet.GetSourceChannel(), data.Denom)
+	if isSource {
 		// sender chain is not the source, unescrow tokens
 
 		// remove prefix added by sender chain
@@ -267,6 +272,9 @@ func (k Keeper) OnRecvPacket(ctx sdk.Context, packet channeltypes.Packet, data t
 			)
 		}()
 
+		if k.hooks != nil {
+			k.hooks.AfterRecvTransfer(ctx, packet.DestinationPort, packet.DestinationChannel, token, data.Receiver, isSource)
+		}
 		return nil
 	}
 
@@ -327,6 +335,9 @@ func (k Keeper) OnRecvPacket(ctx sdk.Context, packet channeltypes.Packet, data t
 		)
 	}()
 
+	if k.hooks != nil {
+		k.hooks.AfterRecvTransfer(ctx, packet.DestinationPort, packet.DestinationChannel, voucher, data.Receiver, isSource)
+	}
 	return nil
 }
 
@@ -374,7 +385,8 @@ func (k Keeper) refundPacketToken(ctx sdk.Context, packet channeltypes.Packet, d
 		return err
 	}
 
-	if types.SenderChainIsSource(packet.GetSourcePort(), packet.GetSourceChannel(), data.Denom) {
+	isSource := types.SenderChainIsSource(packet.GetSourcePort(), packet.GetSourceChannel(), data.Denom)
+	if isSource {
 		// unescrow tokens back to sender
 		escrowAddress := types.GetEscrowAddress(packet.GetSourcePort(), packet.GetSourceChannel())
 		if err := k.bankKeeper.SendCoins(ctx, escrowAddress, sender, sdk.NewCoins(token)); err != nil {
@@ -385,6 +397,9 @@ func (k Keeper) refundPacketToken(ctx sdk.Context, packet channeltypes.Packet, d
 			return sdkerrors.Wrap(err, "unable to unescrow tokens, this may be caused by a malicious counterparty module or a bug: please open an issue on counterparty module")
 		}
 
+		if k.hooks != nil {
+			k.hooks.AfterRefundTransfer(ctx, packet.SourcePort, packet.SourceChannel, token, data.Sender, isSource)
+		}
 		return nil
 	}
 
@@ -399,6 +414,9 @@ func (k Keeper) refundPacketToken(ctx sdk.Context, packet channeltypes.Packet, d
 		panic(fmt.Sprintf("unable to send coins from module to account despite previously minting coins to module account: %v", err))
 	}
 
+	if k.hooks != nil {
+		k.hooks.AfterRefundTransfer(ctx, packet.SourcePort, packet.SourceChannel, token, data.Sender, isSource)
+	}
 	return nil
 }
 
